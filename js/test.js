61e74c2dc0b85f0a65d46c83144a12098e6a72d9
ivanrmn <ivanrmnh@gmail.com>
2018-06-14 21:37:42 +0200

    Añadidas funciones crear y corregir Select

--- js/test.js
+++ js/test.js
@@ -89,6 +89,7 @@ function corregirRadio(x) {
 
     var radio = document.getElementsByName(x);
     var isNull = true;
+
     for (var z = 0, length = radio.length; z < length; z++) {
 
         if (radio[z].checked) {
@@ -108,3 +109,67 @@ function corregirRadio(x) {
     }
 }
 
+function crearSelect(i) {
+    var numSol = xmlDoc.getElementsByTagName('pregunta')[i].getElementsByTagName('respuesta').length;
+    var element = document.getElementById("myForm");
+
+    var div = document.createElement("div");
+    div.setAttribute("id", "div" + i);
+    div.setAttribute("class", "pregunta");
+    element.appendChild(div);
+
+    var enunciado = document.createElement("label");
+    enunciado.setAttribute('for', i);
+    enunciado.innerHTML = xmlDoc.getElementsByTagName('pregunta')[i].getElementsByTagName('enunciado')[0].innerHTML + "<br>";
+    div.appendChild(enunciado);
+
+    var select = document.createElement("select");
+    select.setAttribute("id", i + "select");
+    select.setAttribute("name", i);
+    div.appendChild(select);
+
+    div.appendChild(enunciado);
+
+    for (var k = 0; k < numSol; k++) {
+
+        var question = xmlDoc.getElementsByTagName('pregunta')[i].getElementsByTagName('respuesta')[k].innerHTML;
+        var option = document.createElement("option");
+
+        option.setAttribute("name", i);
+        option.setAttribute("value", k);
+        option.setAttribute('id', k + "check");
+        option.innerHTML = question;
+        select.appendChild(option);
+
+        var label = document.createElement('label');
+        label.setAttribute('for', i);
+    }
+    label.innerHTML = "<br>";
+
+    div.appendChild(label);
+}
+
+function corregirSelect(x) {
+
+    var option = document.getElementsByName(x);
+    var isNull = true;
+
+    for (var z = 0, length = option.length; z < length; z++) {
+        if (option[z].selected){
+            var pregRespuesta = document.getElementById(x + "select").value;
+            var resp = xmlDoc.getElementsByTagName("pregunta")[x].getElementsByTagName("respuesta")[pregRespuesta].getAttribute("correcto");
+
+        if (resp) {
+            document.getElementById("div"+x).style.backgroundColor="green";
+            resultados++;   
+        }
+        else {
+            document.getElementById("div"+x).style.backgroundColor="red";
+        }
+            break;
+        }
+        if(isNull){
+            document.getElementById("div"+x).style.color = "red";
+    }
+    }
+}
